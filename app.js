563c5650fadadff8694d173731f7fe9231772ecc
Kumar Bijayananda <kumarbijayananda@gmail.com>
2025-01-25 16:37:33 -0500

    added geonames fetch to validate and country codes

--- app.js
+++ app.js
@@ -11,52 +11,53 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 var popup = L.popup();
 
 function onMapClick(e) {
-    
-  popup.setLatLng(e.latlng)
-      .setContent("You clicked the map at " + e.latlng.toString())
-      .openOn(map);
-    console.log(e.latlng);
-  var marker = L.marker(e.latlng).addTo(map);
+  checkIfCorrect(e.latlng);
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(map);
+  console.log(e.latlng);
+  //   var marker = L.marker(e.latlng).addTo(map);
 }
 map.on("click", onMapClick);
 
 //---------------------js for map end ---------------------------
 
 let countryList = getCountryList();
-
+let countryCode = "";
 const startButton = document.getElementById("startButton");
 // const hintList=document.querySelectorAll("hint")
-const hint1=document.getElementById("hint1")
-const hint2=document.getElementById("hint2")
-const hint3=document.getElementById("hint3")
-
+const hint1 = document.getElementById("hint1");
+const hint2 = document.getElementById("hint2");
+const hint3 = document.getElementById("hint3");
 
 startButton.addEventListener("click", startGame);
 
 async function startGame() {
-    const countryIndex = Math.floor(Math.random() * countryList.length)
-  const country = countryList[countryIndex].name.common;
-  countryList.splice(countryIndex,1)
+  const countryIndex = Math.floor(Math.random() * countryList.length);
+  country = countryList[countryIndex].name.common;
+  countryCode = countryList[countryIndex].cca2;
+  console.log("here is the country code: "+countryCode);
+  countryList.splice(countryIndex, 1);
+
   const countryInfo = await getInfo(country);
   console.log(countryInfo[0].name.common);
 
-  const flag=document.createElement("img")
+  const flag = document.createElement("img");
   flag.src = countryInfo[0].flags.png;
-  hint1.appendChild(flag)
+  hint1.appendChild(flag);
 
-
-  const capital=document.createElement("h4")
+  const capital = document.createElement("h4");
   capital.textContent = `Capital city: ${countryInfo[0].capital}`;
-  hint1.appendChild(capital)
+  hint1.appendChild(capital);
 
-  const continent=document.createElement("h4")
+  const continent = document.createElement("h4");
   continent.textContent = `Continent: ${countryInfo[0].continents}`;
-  hint2.appendChild(continent)
+  hint2.appendChild(continent);
 
-  const population=document.createElement("h4")
+  const population = document.createElement("h4");
   population.textContent = `Population: ${countryInfo[0].population}`;
-  hint2.appendChild(population)
-
+  hint2.appendChild(population);
 }
 
 // function getCountry() {
@@ -68,7 +69,7 @@ async function startGame() {
 async function getCountryList() {
   try {
     const response = await fetch(
-      `https://restcountries.com/v3.1/all?fields=name`
+      `https://restcountries.com/v3.1/all?fields=name,cca2`
     );
     const data = await response.json();
     countryList = data;
@@ -89,5 +90,19 @@ async function getInfo(country) {
     return data;
   } catch (error) {
     console.log(`Error : ${error}`);
+  }
+}
+
+async function checkIfCorrect(e) {
+  console.log(`LAT: ${e.lat} and LONG: ${e.lng}`);
+
+  try {
+    const response = await fetch(
+      `http://api.geonames.org/countryCodeJSON?lat=${e.lat}&lng=${e.lng}&username=kumarbijayananda`
+    );
+    const data = await response.json();
+    console.log(data.countryName);
+  } catch (error) {
+    console.log("Error from checkIfCorrect:" + error);
   }
 }
